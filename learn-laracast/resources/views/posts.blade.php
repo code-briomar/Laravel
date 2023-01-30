a92d3d3dc87e82311733d7367c9118495c9705b3
Briane <kapolonbraine@gmail.com>
2023-01-30 15:04:44 +0300

    Just some tinkering around with the views and routes

---
+++ learn-laracast/resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Briane's Laravel App</title>
+    <link rel="stylesheet" href="pretty.css">
+</head>
+
+<body>
+    <article>
+        <h1>My First Article</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia modi libero cupiditate quae nobis unde obcaecati blanditiis laudantium nisi sapiente veniam accusantium consectetur fuga molestiae, nulla hic officia velit sequi!</p>
+    </article>
+
+    <article>
+        <h1>My Second Article</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia modi libero cupiditate quae nobis unde obcaecati blanditiis laudantium nisi sapiente veniam accusantium consectetur fuga molestiae, nulla hic officia velit sequi!</p>
+    </article>
+
+    <article>
+        <h1>My Third Article</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia modi libero cupiditate quae nobis unde obcaecati blanditiis laudantium nisi sapiente veniam accusantium consectetur fuga molestiae, nulla hic officia velit sequi!</p>
+    </article>
+
+</body>
+
+</html>
